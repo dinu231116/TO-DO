de17ae8cb3123ae9b165da99ea1b260e10167ccc
dinu231116 <dinudinisha2003@gmail.com>
2025-04-11 14:23:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Load tasks from localStorage when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    loadTasks();
+});
+
+// Add a new task
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.ariaValueMaxlue.trem();
+    if
+
+
+     
